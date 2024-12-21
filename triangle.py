f77cd7aa72f190fe122b003851d1889c3496d62b
Volkov Vitalii <81337496+VOLKIMIR@users.noreply.github.com>
2024-12-21 04:32:17 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -3,10 +3,10 @@ def area(side_a, side_b, side_c):
 
     semi_perimeter = (side_a + side_b + side_c) / 2
     return (
-            semi_perimeter
-            * (semi_perimeter - side_a)
-            * (semi_perimeter - side_b)
-            * (semi_perimeter - side_c)
+        semi_perimeter
+        * (semi_perimeter - side_a)
+        * (semi_perimeter - side_b)
+        * (semi_perimeter - side_c)
     ) ** 0.5
 
 
