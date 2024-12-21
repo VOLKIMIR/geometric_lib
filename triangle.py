f32bc14427738c905ddd20d497fc6602df1eb5a2
Volkov Vitalii <81337496+VOLKIMIR@users.noreply.github.com>
2024-12-21 04:49:00 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,10 +1,11 @@
 def area(side_a, side_b, side_c):
     assert all(x > 0 for x in [side_a, side_b, side_c]), "Side lengths >0"
 
-    semi_perimeter = (side_a + side_b + side_c) / 2
+    semi_per = (side_a + side_b + side_c) / 2
     return (
-        semi_perimeter * (semi_perimeter - side_a) * (semi_perimeter - side_b) * (semi_perimeter - side_c)
+        semi_per * (semi_per - side_a) * (semi_per - side_b)* (semi_per - side_c)
     ) ** 0.5
+
 
 def perimeter(side_a, side_b, side_c):
     assert all(x > 0 for x in [side_a, side_b, side_c]), "Side lengths >0"
