add10827cf4e607e3c93bd5176c538f06cf55351
Volkov Vitalii <81337496+VOLKIMIR@users.noreply.github.com>
2024-12-21 04:26:44 +0300

    Update test_triangle.py

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -1,7 +1,6 @@
 import unittest
 from triangle import area, perimeter
 
-
 class TestTriangle(unittest.TestCase):
     def test_area_with_valid_sides(self):
         # Arrange
@@ -15,11 +14,17 @@ class TestTriangle(unittest.TestCase):
 
     def test_area_with_invalid_sides_raises_assertion_error(self):
         # Arrange
-        side_a, side_b, side_c = 1, 2, 10
+        invalid_sides = [
+            (1, 2, 10),   # Не выполняется неравенство треугольника
+            (-1, 2, 3),   # Одна из сторон отрицательная
+            (1, -2, 3),   # Одна из сторон отрицательная
+            (1, 2, -3),   # Одна из сторон отрицательная
+            (1, 10, 12),  # Не выполняется неравенство треугольника
+        ]
 
-        # Act & Assert
-        with self.assertRaises(AssertionError):
-            area(side_a, side_b, side_c)
+        for sides in invalid_sides:
+            with self.assertRaises(AssertionError):
+                area(*sides)
 
     def test_perimeter_with_valid_sides(self):
         # Arrange
@@ -38,7 +43,6 @@ class TestTriangle(unittest.TestCase):
         # Act & Assert
         with self.assertRaises(AssertionError):
             perimeter(side_a, side_b, side_c)
-
 
 if __name__ == "__main__":
     unittest.main()
